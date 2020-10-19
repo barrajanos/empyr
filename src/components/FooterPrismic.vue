3a10d1e981a7743c6a3c4eff12373c46f3d947bd
barrajanos <59731227+barrajanos@users.noreply.github.com>
2020-10-19 18:50:47 +0200

    r
    rr

---
+++ src/components/FooterPrismic.vue
@@ -0,0 +1,19 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'FooterPrismic'
+}
+</script>
+
+<style scoped>
+.footer {
+  padding: 40px 0;
+  text-align: center;
+}
+
+.logo {
+  width: 140px;
+}
+</style>
